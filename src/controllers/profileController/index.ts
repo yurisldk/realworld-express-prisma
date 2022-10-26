24d9f9df43ea8358471ba41e1fa484d7cf679a01
Ronan Pardo Soares <ronan.soares@gmail.com>
2022-10-26 09:54:21 -0300

    Add follow profile route

--- src/controllers/profileController/index.ts
+++ src/controllers/profileController/index.ts
@@ -1,1 +1,2 @@
 export { default as getProfile } from "./getProfile";
+export { default as followProfile } from "./followProfile";
--- src/routes/api/profiles.ts
+++ src/routes/api/profiles.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
-import { getProfile } from "../../controllers/profileController";
-import { optionalAuthenticate } from "../../middleware/auth/authenticator";
+import { followProfile, getProfile } from "../../controllers/profileController";
+import {
+  authenticate,
+  optionalAuthenticate,
+} from "../../middleware/auth/authenticator";
 
 const router = Router();
 
 router.get("/:username", optionalAuthenticate, getProfile);
 
-router.post("/:username/follow", function (_req, res) {
-  return res.sendStatus(501);
-});
+router.post("/:username/follow", authenticate, followProfile);
 
 router.delete("/:username/follow", function (_req, res) {
   return res.sendStatus(501);
